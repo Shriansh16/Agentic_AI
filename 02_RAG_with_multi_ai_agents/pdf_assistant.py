5ae20d3b3443e5ad21df3b8927074184c194f26c
Shriansh16 <shrianshsingh20@gmail.com>
2025-01-07 22:30:06 +0530

    updated

--- 02_RAG_with_multi_ai_agents/pdf_assistant.py
+++ 02_RAG_with_multi_ai_agents/pdf_assistant.py
@@ -1,6 +1,6 @@
 import typer
 from typing import Optional,List
 from phi.assistant import Assistant        #it will assist us in different tasks
-from phi.storage.assistant.postgres import PgAssistantStorage
-from phi.knowledge.pdf import PDFUrlKnowledgeBase
-from phi.vectordb.pgvector import PgVector2
+from phi.storage.assistant.postgres import PgAssistantStorage   #for managing and storing data in a PostgreSQL database
+from phi.knowledge.pdf import PDFUrlKnowledgeBase    #for managing and storing data in a PostgreSQL database
+from phi.vectordb.pgvector import PgVector2    #for working with vector databases, specifically leveraging the pgvector extension for PostgreSQL. It allows for efficient storage and retrieval of vector embeddings
