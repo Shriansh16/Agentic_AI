cf61f70d9140ce004567336a4efe475442cfe6de
Shriansh16 <shrianshsingh20@gmail.com>
2025-01-13 22:45:37 +0530

    updated

--- 02_RAG_with_multi_ai_agents/pdf_assistant.py
+++ 02_RAG_with_multi_ai_agents/pdf_assistant.py
@@ -3,5 +3,9 @@ from typing import Optional,List
 from phi.assistant import Assistant        #it will assist us in different tasks
 from phi.storage.assistant.postgres import PgAssistantStorage   #for managing and storing data in a PostgreSQL database
 from phi.knowledge.pdf import PDFUrlKnowledgeBase    #for managing and storing data in a PostgreSQL database
-
 from phi.vectordb.pgvector import PgVector2    #for working with vector databases, specifically leveraging the pgvector extension for PostgreSQL. It allows for efficient storage and retrieval of vector embeddings
+import os
+from dotenv import load_dotenv
+load_dotenv
+
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
